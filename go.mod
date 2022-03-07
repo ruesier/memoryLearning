06a6767a921861650942c4b24ae0c94e2c9a5bcb
Devon Call <callbrentdevon@gmail.com>
2022-03-06 20:28:12 -0800

    implemented predict and reset for model

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/ruesier/memoryLearning
 
 go 1.16
+
+require github.com/ruesier/complexMatrix v0.2.0
--- model.go
+++ model.go
@@ -1,6 +1,51 @@
 package memorylearning
 
-type Model interface {
-	Predict(data []float64) []float64
-	Reset()
+import "github.com/ruesier/complexMatrix"
+
+type Model struct {
+	memory         []float64
+	default_memory []float64
+	inputLayer     [][]float64
+	outputLayer    [][]float64
+	memoryLayer    [][]float64
+	internalLayers []complexMatrix.M
+	sigmoid        func(complex128, int, int, complexMatrix.M) complex128
+}
+
+func vectorDot(vec []float64, matrix [][]float64) []float64 {
+	result := make([]float64, len(matrix[0]))
+	for i := range result {
+		for j := range matrix {
+			result[i] += vec[j] * matrix[j][i]
+		}
+	}
+	return result
+}
+
+func (m *Model) Predict(data []float64) []float64 {
+	if len(data)+1 != len(m.inputLayer) {
+		panic("input data is incorrect size for prediction")
+	}
+	data = append(data, 1)
+	data = vectorDot(data, m.inputLayer)
+
+	combined := complexMatrix.CombineIntoMutable([][]float64{data}, [][]float64{m.memory})
+
+	for _, layer := range m.internalLayers {
+		combined = combined.Dot(layer)
+		combined = combined.Map(m.sigmoid)
+	}
+
+	dataMatrix, memMatrix := complexMatrix.Parts(combined)
+	data = vectorDot(dataMatrix[0], m.outputLayer)
+	data = data[:len(data)-1]
+
+	m.memory = vectorDot(memMatrix[0], m.memoryLayer)
+
+	return data
+}
+
+func (m *Model) Reset() {
+	m.memory = make([]float64, 0, len(m.default_memory))
+	copy(m.memory, m.default_memory)
 }
