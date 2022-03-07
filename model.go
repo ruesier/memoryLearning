af06e4b036d70dc0a8d460a6cf23d40a254171a3
Devon Call <callbrentdevon@gmail.com>
2022-03-06 23:51:12 -0800

    implemented tanh activation

--- model.go
+++ model.go
@@ -1,15 +1,16 @@
 package memorylearning
 
 import "github.com/ruesier/complexMatrix"
+import "math"
+import "math/cmplx"
 
 type Model struct {
-	memory         []float64
-	default_memory []float64
-	inputLayer     [][]float64
-	outputLayer    [][]float64
-	memoryLayer    [][]float64
-	internalLayers []complexMatrix.M
-	sigmoid        func(complex128, int, int, complexMatrix.M) complex128
+	memory             []float64
+	default_memory     []float64
+	inputLayer         [][]float64
+	outputLayer        [][]float64
+	memoryLayer        [][]float64
+	internalLayers     []complexMatrix.M
 }
 
 func vectorDot(vec []float64, matrix [][]float64) []float64 {
@@ -28,19 +29,28 @@ func (m *Model) Predict(data []float64) []float64 {
 	}
 	data = append(data, 1)
 	data = vectorDot(data, m.inputLayer)
+	for i := 0; i < len(data) - 1; i++ {
+		data[i] = math.Tanh(data[i])
+	}
 
 	combined := complexMatrix.CombineIntoMutable([][]float64{data}, [][]float64{m.memory})
 
 	for _, layer := range m.internalLayers {
 		combined = combined.Dot(layer)
-		combined = combined.Map(m.sigmoid)
+		combined = combined.Map(tanh_activation_complex)
 	}
 
 	dataMatrix, memMatrix := complexMatrix.Parts(combined)
 	data = vectorDot(dataMatrix[0], m.outputLayer)
 	data = data[:len(data)-1]
+	for i := range data {
+		data[i] = math.Tanh(data[i])
+	}
 
 	m.memory = vectorDot(memMatrix[0], m.memoryLayer)
+	for i := 0; i < len(m.memory) - 1; i++ {
+		m.memory[i] = math.Tanh(m.memory[i])
+	}
 
 	return data
 }
@@ -48,4 +58,13 @@ func (m *Model) Predict(data []float64) []float64 {
 func (m *Model) Reset() {
 	m.memory = make([]float64, 0, len(m.default_memory))
 	copy(m.memory, m.default_memory)
+}
+
+func tanh_activation_complex(val complex128, col int, row int, matrix complexMatrix.M) complex128 {
+	magnitude, theta := cmplx.Polar(val)
+	if W, H := matrix.Dim(); row == H - 1 || col == W - 1 {
+		return cmplx.Rect(magnitude, theta)
+	} else {
+		return cmplx.Rect(math.Tanh(magnitude), theta)
+	}
 }
